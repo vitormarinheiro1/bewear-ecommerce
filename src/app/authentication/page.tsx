92e8ee094cdc31dd04d471a03f9260210bf50e3b
Vitor Marinheiro <vitorrodrigues@prefeitura.sp.gov.br>
2025-08-06 12:21:34 -0300

    feat: add sign-in and sign-up login

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,23 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import SignInForm from "./components/sign-in-form";
+import SignUpForm from "./components/sign-up-form";
+
+export default async function Authentication() {
+  return (
+    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
+      <Tabs defaultValue="sign-in">
+        <TabsList>
+          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+          <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="sign-in">
+          <SignInForm />
+        </TabsContent>
+        <TabsContent value="sign-up">
+          <SignUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
