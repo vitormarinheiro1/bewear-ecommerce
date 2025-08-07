083df56adc851086810394c4357637384b740d26
Vitor Marinheiro <vitorrodrigues@prefeitura.sp.gov.br>
2025-08-07 07:56:03 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,44 @@
-import Header from "@/components/commom/header";
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
-export default function Home() {
+import Header from "@/components/commom/header";
+import ProductList from "@/components/commom/product-list";
+import { db } from "@/db";
+
+export default async function Home() {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
   return (
     <>
       <Header />
-      <Button>Bootcamp</Button>
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          <Image
+            src="/banner-02.png"
+            alt="Seja autêntico"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/commom/product-list.tsx
@@ -0,0 +1,27 @@
+import { productTable, productVariantTable } from "@/db/schema";
+
+import ProductItem from "./product-item";
+
+interface ProductListProps {
+  title: string;
+  products: (typeof productTable.$inferSelect & {
+    variants: (typeof productVariantTable.$inferSelect)[];
+  })[];
+}
+
+const ProductList = async ({ title, products }: ProductListProps) => {
+  //   const products = await db.query.productTable.findMany();
+  //   console.log(products);
+  return (
+    <div className="space-y-6">
+      <h3 className="px-5 font-semibold">{title}</h3>
+      <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
